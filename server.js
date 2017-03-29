6a72a908c28388809582f0976961ddd260a26f45
Gavin Chan <gavinchan267@gmail.com>
2017-03-29 17:45:37 +0800

    web server改为express

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 /**
  * Created by gavin on 3/25/17.
  */
-var path = require('path');
+
 var assert = require('assert');
 var fs = require('fs');
-var restify = require('restify');
+var path = require('path');
 
+var server = require('./lib/https-enforcer');
 
 function loadConfig(file) {
 	assert.ok(file);
@@ -18,24 +19,7 @@ function loadConfig(file) {
 var cfgFile = path.join(__dirname, '/etc/config.json');
 var cfg = loadConfig(cfgFile);
 
-var log = require('bunyan').createLogger({
-	name: 'adminui',
-	level: process.env.LOG || cfg.logLevel || 'info',
-	serializers: restify.bunyan.serializers
-});
-
-
-log.info('初始化AdminUI');
-var adminui = require('./lib/adminui').createServer({
-	config: cfg,
-	log: log,
-	version: require('./package.json').version
-});
-
-adminui.listen(function ready() {
-	log.info('--------------开始监听---------------');
-});
-
-process.on('uncaughtException', function preventOtherError(e) {
-	log.fatal(e, '未捕获的异常');
-});
+/**
+ * Create HTTP server.
+ */
+server.createServer(cfg);
